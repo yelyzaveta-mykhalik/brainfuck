803547317267e4baf19117238005f5946d80c4b6
Liza <lizamikhalik1402@gmail.com>
2022-03-25 09:33:59 +0100

    Added additional description

--- brainfuck/doc.go
+++ brainfuck/doc.go
@@ -3,6 +3,8 @@ Package brainfuck contains functionality to interpret any program
 written in Brainfuck language.
 Usage:
 Put a file with program in Brainfuck language into directory "examples",
-then run it from CLI
+then run it from terminal via command 'go run ./. examples/example.bf'.
+You can use your own file instead of example.bf, in this case put its
+in command instead of 'example.bf'
 */
 package brainfuck
