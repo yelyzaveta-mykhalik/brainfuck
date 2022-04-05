c5e124171e6dab390d1388b6bcdd56b659c9fa51
Liza <lizamikhalik1402@gmail.com>
2022-04-05 02:44:34 +0200

    Main file was changed

--- main.go
+++ main.go
@@ -1,28 +1,13 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
-	"os"
-
 	"github.com/yelyzaveta-mykhalik/brainfuck/brainfuck"
 )
 
 func main() {
 
-	//Getting file name from CLI, reading that file
-	//and executing context of file
-	fileName := os.Args[1]
-	code, err := ioutil.ReadFile(fileName)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "error: %s\n", err)
-		os.Exit(-1)
-	}
+	const helloWorld = "++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>."
 
-	compiler := brainfuck.NewCompiler(string(code))
-	instructions := compiler.Compile()
-
-	m := brainfuck.NewInterpreter(instructions, os.Stdin, os.Stdout)
-	m.Execute()
+	brainfuck.Execute(helloWorld)
 
 }
